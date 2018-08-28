e0d800956d3e59add067f2760181b3ae66c6e3c5
Renju Radhakrishnan <renju467@gmail.com>
2018-08-27 20:37:52 -0400

    Example postgresql query

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"database/sql"
+
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+	_ "github.com/lib/pq"
 	"github.com/renjuju/auth/dao"
 	"github.com/renjuju/auth/encryption"
 	"github.com/renjuju/auth/models"
+	"github.com/sirupsen/logrus"
 )
+
+type User struct {
+	Username string
+	Password string
+}
 
 func main() {
 	router := echo.New()
@@ -30,4 +39,28 @@ func main() {
 	router.POST("/api/login", encryptionHandler.Login)
 
 	router.Logger.Fatal(router.Start(":8080"))
+}
+
+func exampleSql() {
+	connStr := "user=postgres dbname=profile sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		logrus.Fatalf("unable to open sql connection %v", err)
+	}
+
+	rows, err := db.Query("SELECT * FROM users")
+	if err != nil {
+		logrus.Fatalf("unable to query db %v", err)
+	}
+
+	for rows.Next() {
+		var user User
+		err := rows.Scan(&user.Username, &user.Password)
+
+		if err != nil {
+			logrus.Fatalf("unable to query columns %v", err)
+		}
+
+		logrus.Infof("user data %v", user)
+	}
 }
